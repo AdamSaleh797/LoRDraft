696b85ee4d0559e5845a6bccd586da0fc9c192a3
adamsaleh <adamsaleh797@gmail.com>
2022-12-30 11:27:48 -0600

    elise goes on the screen

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,9 +3,56 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css'
 
+interface Card {
+  rarity: string;
+  imageUrl: string;
+  cost: number;
+  name: string;
+  regions: string[];
+  subtypes: string[];
+}
+
+interface CardProps {
+  card: Card
+}
+
+interface CardState {
+  card: Card
+}
+
+class Cards extends React.Component<CardProps, CardState> {
+  constructor(props: CardProps) {
+    super(props);
+    this.state = {
+      card: props.card,
+    }
+  }
+
+  render() {
+    return (
+      <div className="card">
+        <img src={this.state.card.imageUrl} alt={this.state.card.name} width={400} height={500}></img>
+      </div>
+    );
+  }
+}
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const testCard: Card = {
+  rarity: "Champion",
+  imageUrl: "http://dd.b.pvp.net/3_21_0/set1/en_us/img/cards/01SI053.png",
+  cost: 2,
+  name: "Elise",
+  regions: ["Shadow Isles"],
+  subtypes: ["Spider"]
+}
+
 root.render(
-  <div className='main'>Hello</div>
+  <Cards card={testCard} />
 );
+
+
