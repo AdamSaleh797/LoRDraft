d2e95c0cbf3cb7426bc844073029c5400dbf8857
claytonknittel <cknittel@wustl.edu>
2023-01-02 04:31:05 -0800

    State machine implementation, used for auth session

--- client/src/auth_session.tsx
+++ client/src/auth_session.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 import { Buffer } from 'buffer'
 import { LoRDraftClientSocket, SessionCred, SessionCredT } from 'socket-msgs'
-import { isOk } from 'lor_util'
+import { isOk, Status } from 'lor_util'
+import { StateMachine, StateMachineDef } from 'state_machine'
 
 const STORAGE_AUTH_INFO_KEY = 'auth_info'
 
@@ -31,7 +32,6 @@ function getStorageAuthInfo(): SessionCred | null {
     }
   )
 
-  SessionCredT.check(auth_info_prim)
   if (!SessionCredT.guard(auth_info_prim)) {
     console.log("session cred doesn't match expected format:")
     console.log(auth_info_prim)
@@ -52,12 +52,37 @@ function getStorageAuthInfo(): SessionCred | null {
 function setStorageAuthInfo(session_cred: SessionCred): void {
   window.sessionStorage.setItem(
     STORAGE_AUTH_INFO_KEY,
-    JSON.stringify(session_cred)
+    JSON.stringify(session_cred, (key, val) => {
+      // if (key === 'buffer') {
+      //   Buffer.
+      // }
+      // else {
+      return val
+      // }
+    })
   )
 }
 
 function clearStorageAuthInfo(): void {
   window.sessionStorage.removeItem(STORAGE_AUTH_INFO_KEY)
+}
+
+interface RegisterComponentProps {
+  register_fn: () => void
+}
+
+export function RegisterComponent(props: RegisterComponentProps) {
+  return (
+    <div>
+      <input
+        value={'clayton'}
+        onChange={(evt) => {
+          console.log(evt)
+        }}
+      />
+      <button onClick={props.register_fn}>Register</button>
+    </div>
+  )
 }
 
 interface LoginComponentProps {
@@ -92,48 +117,136 @@ interface SessionComponentProps {
   socket: LoRDraftClientSocket
 }
 
+const enum SessionState {
+  LOGIN = 'LOGIN',
+  REGISTER = 'REGISTER',
+  SIGNED_IN = 'SIGNED_IN',
+}
+
 export function SessionComponent(props: SessionComponentProps) {
   const [username, setUsername] = React.useState<string | null>(null)
+  const [sessionState, setSessionState] = React.useState<SessionState>(
+    SessionState.LOGIN
+  )
 
-  const socket = props.socket
-
-  React.useEffect(() => {
-    socket.on('register_res', (status) => {
-      if (!isOk(status)) {
-        console.log(status)
-      }
-    })
-
-    socket.on('login_res', (status, session_cred) => {
-      console.log(status)
-
-      if (isOk(status) && session_cred !== undefined) {
-        // Explicitly convert token to a buffer, as it is actually deserialized as an ArrayBuffer.
-        session_cred.token = Buffer.from(session_cred.token)
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const machine_def = {
+    [SessionState.LOGIN]: {
+      [SessionState.REGISTER]: () => {
+        console.log('login -> register')
+      },
+      [SessionState.SIGNED_IN]: (session_cred: SessionCred) => {
+        console.log('login -> signed in')
         console.log('saving token to session storage')
         setStorageAuthInfo(session_cred)
         setUsername(session_cred.username)
-      }
-    })
+      },
+    },
+    [SessionState.REGISTER]: {
+      [SessionState.LOGIN]: () => {
+        console.log('register -> login')
+      },
+    },
+    [SessionState.SIGNED_IN]: {
+      [SessionState.LOGIN]: () => {
+        console.log('signed in -> login')
+        clearStorageAuthInfo()
+        setUsername(null)
+      },
+    },
+  } as const
 
-    socket.on('join_session_res', (status, session_cred) => {
-      if (isOk(status) && session_cred !== undefined) {
-        console.log('joined session')
-        setUsername(session_cred.username)
-      } else {
+  const handle_register_res = React.useRef<(status: Status) => void>(
+    () => undefined
+  )
+  const handle_login_res = React.useRef<
+    (status: Status, session_cred?: SessionCred) => void
+  >(() => undefined)
+  const handle_join_session_res = React.useRef<
+    (status: Status, session_cred?: SessionCred) => void
+  >(() => undefined)
+  const handle_logout_res = React.useRef<(status: Status) => void>(
+    () => undefined
+  )
+
+  const session_state_machine = new StateMachine(
+    machine_def,
+    sessionState,
+    setSessionState
+  )
+
+  handle_register_res.current = (status) => {
+    if (!isOk(status)) {
+      console.log(status)
+      return
+    }
+
+    console.log('registered!')
+    session_state_machine.transition(SessionState.REGISTER, SessionState.LOGIN)
+  }
+
+  handle_login_res.current = (status, session_cred) => {
+    if (!isOk(status) || session_cred === undefined) {
+      console.log(status)
+      return
+    }
+    session_cred.token = Buffer.from(session_cred.token)
+
+    console.log('logged in!')
+    session_state_machine.transition(
+      SessionState.LOGIN,
+      SessionState.SIGNED_IN,
+      session_cred
+    )
+  }
+
+  handle_join_session_res.current = (status, session_cred) => {
+    if (!isOk(status) || session_cred === undefined) {
+      if (sessionState === SessionState.LOGIN) {
         console.log('failed to join session')
         console.log(status)
         console.log('clearing token session storage')
         clearStorageAuthInfo()
       }
+      return
+    }
+    session_cred.token = Buffer.from(session_cred.token)
+
+    console.log('joined session')
+    session_state_machine.transition(
+      SessionState.LOGIN,
+      SessionState.SIGNED_IN,
+      session_cred
+    )
+  }
+
+  handle_logout_res.current = (status) => {
+    if (!isOk(status)) {
+      console.log(status)
+      return
+    }
+
+    console.log('logged out!')
+    session_state_machine.transition(SessionState.SIGNED_IN, SessionState.LOGIN)
+  }
+
+  const socket = props.socket
+
+  React.useEffect(() => {
+    socket.on('register_res', (status) => {
+      handle_register_res.current(status)
+    })
+
+    socket.on('login_res', (status, session_cred) => {
+      handle_login_res.current(status, session_cred)
+    })
+
+    socket.on('join_session_res', (status, session_cred) => {
+      handle_join_session_res.current(status, session_cred)
     })
 
     socket.on('logout_res', (status) => {
-      console.log('logout response:')
-      console.log(status)
-
-      clearStorageAuthInfo()
-      setUsername(null)
+      handle_logout_res.current(status)
     })
   }, [])
 
---
+++ common/state_machine.ts
@@ -0,0 +1,57 @@
+type KeyT = string | number | symbol
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type AnyFn = (...args: any) => void
+
+export type StateMachineStateDef<State extends KeyT> = Partial<
+  Record<State, AnyFn>
+>
+
+export type StateMachineDef<State extends KeyT> = {
+  [StateInst in State]: StateMachineStateDef<State>
+}
+
+export class StateMachine<
+  MachineDef extends StateMachineDef<State>,
+  State extends KeyT
+> {
+  state_machine_def: MachineDef
+  state: State
+  state_update_fn?: (state: State) => void
+
+  constructor(
+    state_machine_def: MachineDef,
+    initial_state: State,
+    state_update_fn?: (state: State) => void
+  ) {
+    this.state_machine_def = state_machine_def
+    this.state = initial_state
+    this.state_update_fn = state_update_fn
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  transition<
+    FromT extends State,
+    ToT extends State,
+    UpdateFnT extends MachineDef extends Record<FromT, infer U>
+      ? U extends Record<ToT, infer V>
+        ? V extends AnyFn
+          ? V
+          : never
+        : never
+      : never
+  >(from: FromT, to: ToT, ...args: Parameters<UpdateFnT>): boolean {
+    if (this.state !== from) {
+      return false
+    }
+
+    const state_def = this.state_machine_def[from]
+    const transition_fn = state_def[to] as UpdateFnT
+    ;(transition_fn as AnyFn)(...args)
+
+    if (this.state_update_fn !== undefined) {
+      this.state_update_fn(to)
+    }
+    this.state = to
+    return true
+  }
+}
