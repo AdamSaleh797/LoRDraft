abcbd1426a12b1f70905f78b239ea805e902f2a1
adamsaleh <adamsaleh797@gmail.com>
2023-01-02 01:10:48 -0600

    made whole website

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,8 +6,16 @@ import { Card } from 'card'
 import { LoRDraftClientSocket } from 'socket-msgs'
 import { SessionComponent } from './auth_session'
 
+import { useState } from 'react'
+import { createReadStream } from 'fs'
+import { builtinModules } from 'module'
+import { numberLiteralTypeAnnotation } from '@babel/types'
+
+const MAX_DISPLAY_COST = 8
+
 interface CardComponentProps {
   card: Card
+  recordCard: (card: Card) => void
 }
 
 function CardComponent(props: CardComponentProps) {
@@ -20,16 +28,30 @@ function CardComponent(props: CardComponentProps) {
     width: '100%',
     height: '100%',
   }
+
+  const record = () => {
+    props.recordCard(props.card)
+  }
+
   return (
-    <div className='card' style={style}>
+    <div className='card' style={style} onClick={record}>
       <img src={props.card.imageUrl} alt={props.card.name} style={img_style} />
     </div>
   )
 }
 
-function PoolComponent() {
+interface PoolComponentProps {
+  recordCard: (card: Card) => void
+}
+
+function PoolComponent(props: PoolComponentProps) {
   const [cards, setCards] = React.useState<Card[]>([])
-  const names = ['Illaoi', 'Norra', 'Gwen', 'Aphelios']
+  const [names, setNames] = React.useState<string[]>([
+    'Illaoi',
+    'Norra',
+    'Gwen',
+    'Aphelios',
+  ])
 
   socket.off('card_res')
   socket.on('card_res', (err, card) => {
@@ -40,7 +62,7 @@ function PoolComponent() {
     }
     console.log(cards.concat([card]))
     setCards(cards.concat([card]))
-    if (cards.length < 3) {
+    if (cards.length < names.length - 1) {
       socket.emit('card_req', names[cards.length + 1])
     }
   })
@@ -49,24 +71,83 @@ function PoolComponent() {
     socket.emit('card_req', names[0])
   }, [])
 
+  const switchPool = () => {
+    setCards([])
+    setNames(['Viego', 'Aatrox', 'Seraphine', 'Vayne'])
+    socket.emit('card_req', 'Viego')
+  }
+
   return (
-    <body>
+    <div onClick={switchPool}>
       {cards.map((card) => {
-        return <CardComponent card={card} />
+        return <CardComponent card={card} recordCard={props.recordCard} />
       })}
-    </body>
+    </div>
+  )
+}
+
+interface ManaCurveComponentProps {
+  cards: Card[]
+}
+
+function ManaCurve(props: ManaCurveComponentProps) {
+  const histogram: number[] = new Array(MAX_DISPLAY_COST + 1).fill(0)
+
+  props.cards.forEach((card: Card) => {
+    histogram[Math.min(card.cost, MAX_DISPLAY_COST)]++
+  })
+
+  return (
+    <div>
+      <div>
+        {histogram.map((manaValueCount) => {
+          const barStyle = {
+            width: `calc(${100 / histogram.length}% - 4px)`,
+            height: `${(manaValueCount / props.cards.length) * 200}px`,
+            backgroundColor: 'blue',
+            display: 'inline-block',
+            borderStyle: 'solid',
+            borderWidth: '2px',
+          }
+
+          return <div className='histogram' style={barStyle}></div>
+        })}
+      </div>
+      <div>
+        {histogram.map((_1, count) => {
+          const textStyle = {
+            width: `calc(${100 / histogram.length}% - 4px)`,
+            'text-align': 'center',
+            display: 'inline-block',
+          }
+
+          return (
+            <div className='label' style={textStyle}>
+              {count}
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
 const socket: LoRDraftClientSocket = io()
 
 function Main() {
-  const Pool: Card[] = []
+  const [cards, setCards] = React.useState<Card[]>([])
 
+  const recordCard = (card: Card) => {
+    setCards(cards.concat(card))
+  }
   return (
     <div>
-      <PoolComponent />
-      <SessionComponent socket={socket} />
+      <div>
+        <PoolComponent recordCard={recordCard} />
+      </div>
+      <div>
+        <ManaCurve cards={cards} />
+      </div>
     </div>
   )
 }
