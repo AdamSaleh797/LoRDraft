72ef1bbd61619f3cbd428e30f8820a0d93b9f8c9
claytonknittel <cknittel@wustl.edu>
2023-01-07 19:20:50 -0800

    Disable lint warnings.

--- server/src/core_bundle.ts
+++ server/src/core_bundle.ts
@@ -1,8 +1,10 @@
 import { Status } from 'lor_util'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const g_core_bundle = ['globals-en_us.json']
 
 export function loadCoreBundle(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   callback: (status: Status) => void = () => undefined
 ) {
   return
