7e1dac05915a59c27cb3a6a6265cd88b189406f6
claytonknittel <cknittel@wustl.edu>
2023-06-09 19:18:43 -0700

    Fix again

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -95,17 +95,13 @@ function Main() {
   }
 
   if (authInfo !== null && gameMetadataRef.current === null) {
-    getGameMetadata(
-      socket_ref.current,
-      authInfo,
-      (status: Status<GameMetadata>) => {
-        if (isOk(status)) {
-          gameMetadataRef.current = status.value
-        } else {
-          console.log(status)
-        }
+    getGameMetadata(socket_ref.current, authInfo, (status) => {
+      if (isOk(status)) {
+        gameMetadataRef.current = status.value
+      } else {
+        console.log(status)
       }
-    )
+    })
   }
 
   const deckInfoDisplay = {
