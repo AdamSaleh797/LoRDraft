9f96b0deb8611e6a08546d0f4ef22b1abeaab1fe
adamsaleh <adamsaleh797@gmail.com>
2023-07-04 01:54:07 -0500

    region filter progress

--- client/src/components/draft/Card.tsx
+++ client/src/components/draft/Card.tsx
@@ -2,28 +2,41 @@ import React from 'react'
 
 import style from './Card.module.css'
 
-import { Card } from 'common/game/card'
+import { Card, isMainRegion } from 'common/game/card'
+import { DraftStateInfo } from 'common/game/draft'
 
 export interface CardComponentProps {
   card: Card | null
   numCards: number
   isSelected: boolean
   select: () => void
+  draftState: DraftStateInfo
 }
 
 export function CardComponent(props: CardComponentProps) {
+  if (props.card === null) {
+    return <div className={style.card} onClick={props.select} />
+  }
+
+  const regions = props.card.regions.filter((region) =>
+    props.draftState.deck.regions.includes(region)
+  )
+  const region = regions[0]
+
   return (
     <div className={style.card} onClick={props.select}>
-      {props.card === null ? (
-        <div />
-      ) : (
-        <img
-          className={props.isSelected ? style.selected : ''}
-          src={props.card.imageUrl}
-          alt={props.card.name}
-          draggable={false}
-        ></img>
-      )}
+      <img
+        className={
+          props.isSelected
+            ? `${style.selected} ${
+                isMainRegion(region) ? style[region] : style.Runeterra
+              }`
+            : ''
+        }
+        src={props.card.imageUrl}
+        alt={props.card.name}
+        draggable={false}
+      ></img>
     </div>
   )
 }
