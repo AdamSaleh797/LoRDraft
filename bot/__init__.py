fc62b4b34c8c37878b5f774428e93e20b855a19d
claytonknittel <cknittel@wustl.edu>
2023-08-06 20:59:36 -0700

    Package manager + formatter

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,23 +1,22 @@
-import discord 
+import discord
 
 
 def run_discord_bot():
     intents = discord.Intents.default()
     intents.message_content = True
 
-    TOKEN = 'MTEzNzkzOTY2MTU2Njc4MzY0OQ.G5A9mW.JZe24xFgnCB2NhIdBbn5QBg89QJrO_xc4eTKR0'
-    client = discord.Client(intents = intents)
+    TOKEN = "MTEzNzkzOTY2MTU2Njc4MzY0OQ.G5A9mW.JZe24xFgnCB2NhIdBbn5QBg89QJrO_xc4eTKR0"
+    client = discord.Client(intents=intents)
 
     @client.event
     async def on_ready():
-        print(f'{client.user} is now running')
+        print(f"{client.user} is now running")
 
     @client.event
     async def on_message(message):
-
         # we do not want the bot to reply to itself
         if message.author.id == client.user.id:
-            return 
+            return
 
         username = str(message.author)
         user_message = str(message.content)
@@ -27,12 +26,7 @@ def run_discord_bot():
 
         await message.channel.send(message.content)
 
-        
-
-
     client.run(TOKEN)
 
 
 run_discord_bot()
-
-    
