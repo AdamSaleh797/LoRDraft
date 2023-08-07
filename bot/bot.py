2c56a94eb30df0d3840da286b446c331bdd804f7
Adam Saleh <65844871+Moniino@users.noreply.github.com>
2023-08-07 00:27:38 -0500

    Add basic queue functionality (#69)
    * functionality and formatting
    
    * fixed everything
    
    * fixed everything
    
    * destroyed linter
    
    * destroyed linter

--- bot/bot.py
+++ bot/bot.py
@@ -1,11 +1,16 @@
+"""Runs Bot"""
+
 import discord
+import responses
+
+TOKEN = "MTEzNzkzOTY2MTU2Njc4MzY0OQ.G5A9mW.JZe24xFgnCB2NhIdBbn5QBg89QJrO_xc4eTKR0"
 
 
 def run_discord_bot():
+    """Runs Bot"""
     intents = discord.Intents.default()
     intents.message_content = True
 
-    TOKEN = "MTEzNzkzOTY2MTU2Njc4MzY0OQ.G5A9mW.JZe24xFgnCB2NhIdBbn5QBg89QJrO_xc4eTKR0"
     client = discord.Client(intents=intents)
 
     @client.event
@@ -20,13 +25,8 @@ def run_discord_bot():
 
         username = str(message.author)
         user_message = str(message.content)
-        channel = str(message.channel)
 
-        print(f"{username} said: '{user_message}' ({channel})")
-
-        await message.channel.send(message.content)
+        response = responses.handle_response(user_message, username)
+        await message.channel.send(response)
 
     client.run(TOKEN)
-
-
-run_discord_bot()
